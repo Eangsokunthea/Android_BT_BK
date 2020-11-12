76da4bc45aef508cf44894b4833803d136b83a7a
Eangsokunthea <sokuntheaeang198@gmail.com>
2020-11-12 22:50:08 +0700

    change

---
+++ TH2020_KTLT_BK/699221_20180280_EANGSOKUNTHEA_BaiTH_02/20180280_EANG-SOKUNTHEA_BaiSo9.cpp
@@ -0,0 +1,18 @@
+/*HOVATEN: EANG SOKUNTHEA
+  MSSV: 20180280*/
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+double sigmoid_slow(double x)
+{
+    return 1.0 / (1.0 + exp(-x));
+}
+
+int main()
+{
+    double xval;
+    cin >> xval;
+    printf("%.2f \n", sigmoid_slow(xval));
+    cout << "Correct answer! Your code is faster at least 30%!";
+}
